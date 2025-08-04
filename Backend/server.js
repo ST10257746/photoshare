471036eb8e92b053a8676f275c502ec83ac290f8
ST10257746 <ST10257746@vcconnect.edu.za>
2025-08-04 15:03:49 +0200

    Set up server

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,23 @@
-console.log('Server has started!')
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+//load env variables
+dotenv.config();
+
+//initialise express app
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Add Basic route
+app.get('/', (req, res) => {
+    res.send('welcome to photoshare app')
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('server is running')
+});
